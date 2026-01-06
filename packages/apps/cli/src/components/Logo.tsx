0100b8dd84c25406558fb65828dbbaac9a3dc987
Calum Webb <calumpeterwebb@icloud.com>
2026-01-05 17:03:26 -0800

    fix(cli): correct ASCII logo spacing

--- packages/apps/cli/src/components/Logo.tsx
+++ packages/apps/cli/src/components/Logo.tsx
@@ -5,7 +5,7 @@ export function Logo(): React.ReactNode {
     <Box flexDirection="column">
       <Text>█▀▀ █ █ █ █▀█ █▀█ █▀█</Text>
       <Text>▀▀█ █▀█ █ █▀▀ █▀▀ ██▀</Text>
-      <Text>▀▀▀ ▀ ▀ ▀ ▀ ▀ ▀ ▀</Text>
+      <Text>▀▀▀ ▀ ▀ ▀ ▀   ▀   ▀ ▀</Text>
     </Box>
   )
 }
